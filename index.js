ea36a7a98388117996ea0d06dcce88620cd97487
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-04-29 23:10:01 +0600

    created get API for specific item query by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,7 +30,14 @@ async function run() {
     app.get('/crafts', async(req, res)=>{
         const cursor = craftsCollection.find();
         const result = await cursor.toArray();
-        res.send(result);
+        res.send(result)
+    })
+
+    app.get('/crafts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await craftsCollection.findOne(query);
+      res.send(result);
     })
 
     app.post('/crafts', async(req, res)=>{
